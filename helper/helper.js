20585418482355203a10f5bb4a0d9cd18b693c76
styang <42330684@qq.com>
2019-11-06 17:33:14 +0800

    fix: lottery function

--- helper/helper.js
+++ helper/helper.js
@@ -20,113 +20,91 @@
     var getComments = function(page){
         return axios.get(baseUrl.replace('PAGE',page))
     }
-    var total = 0
     var totalPage = 0
     var comments = []
-    var results = []
-    var uids = new Set()
     var users = new Set()
-    // 重复回复的楼层
-    var excludeFloor = new Set()
-    var ram = function(){
-        var val = Math.ceil(Math.random()*total);
-        if(results.indexOf(val)<0 && !excludeFloor.has(val)){
-            results.push(val);
-            return val
-        }else if(results.length === users.size){
-
-        }else{
-            ram();
-        }
-    };
     var luckcount = 1
     window.lottery = function(){
-        if( luckcount === users.size ){
-            return false
+     if(comments.length > 0){
+        let commentsindex = Math.floor(Math.random() * comments.length)
+        if(comments.length === 1){
+            commentsindex = 0
         }
-        let floornumber = ram()
-        console.log(`抽取第${luckcount}位天选之子${floornumber}`)
-        let user = comments.find(e=>e.floor === floornumber)
-        if( user && !uids.has(user.userId) && !user.isUp ){
-            console.log(`第${luckcount}位天选之子【${ user.userName}】`)
-            if($('.edui-body-container').text().indexOf('评论5字起') >= 0){
-                $('.edui-body-container').empty()
-            }
-            $('.edui-body-container').append(`<p>第${luckcount}位天选之子${floornumber}楼的 @${user.userName}</p>`)
-            luckcount++
-            uids.add(user.userId)
-        } else if(results.length !== users.size){
-            window.lottery()
+        let user = comments[commentsindex]
+        comments.splice(commentsindex,1)
+        console.log(`第${luckcount}位天选之子【${ user.userName}】`)
+        if($('.edui-body-container').text().indexOf('评论5字起') >= 0){
+            $('.edui-body-container').empty()
         }
+        $('.edui-body-container').append(`<p>第${luckcount}位天选之子${user.floor}楼的 @${user.userName}</p>`)
+        luckcount++
     }
-    $(function(){
-        getComments(1).then(res =>{
-            const data = res.data
-            if(data.isUp){
-                console.log('确认为up主，载入抽奖助手...')
-                total = data.totalCount
-                totalPage = data.totalPage
-                for(var key in data.commentsMap){
-                    var comment = data.commentsMap[key]
-                    if(!users.has(comment.userId) && !comment.isUp){
-                        users.add(comment.userId)
-                        comments.push(comment)
-                    }else {
-                        excludeFloor.add(comment.floor)
-                    }
-                }
-                for(var page = 2; page < totalPage + 1; page ++ ){
-                    getComments(page).then(pageres => {
-                        for(var key in pageres.data.commentsMap){
-                            var comment = pageres.data.commentsMap[key]
-                            if(!users.has(comment.userId) && !comment.isUp){
-                                users.add(comment.userId)
-                                comments.push(comment)
-                            }else {
-                                excludeFloor.add(comment.floor)
-                            }
-                        }
-                    })
-                }
-                var button = document.createElement('div');
-                var jqButton = $(button);
-                var buttonCss = {
-                    'border-radius':'15px',
-                    'padding':'5px 10px',
-                    'background':'#fd4c5b',
-                    'display':'inline-block',
-                    'cursor':'pointer',
-                    'margin-left':'20px',
-                    'color':'#fff'
-                };
-                if(video){
-                    buttonCss.margin = '20px 5px'
-                }
-                jqButton.css(buttonCss);
-                jqButton.attr('onclick',"lottery()");
-                jqButton.text('开始抽奖');
-                $('#art-operate').append(button.outerHTML);
-                if(video){
-                    $('.banana').after(button.outerHTML);
+
+}
+$(function(){
+    getComments(1).then(res =>{
+        const data = res.data
+        if(data.isUp){
+            console.log('确认为up主，载入抽奖助手...')
+            totalPage = data.totalPage
+            for(var key in data.commentsMap){
+                var comment = data.commentsMap[key]
+                if(!users.has(comment.userId) && !comment.isUp){
+                    users.add(comment.userId)
+                    comments.push(comment)
                 }
             }
-        })
+            for(var page = 2; page < totalPage + 1; page ++ ){
+                getComments(page).then(pageres => {
+                    for(var key in pageres.data.commentsMap){
+                        var comment = pageres.data.commentsMap[key]
+                        if(!users.has(comment.userId) && !comment.isUp){
+                            users.add(comment.userId)
+                            comments.push(comment)
+                        }
+                    }
+                })
+            }
+            var button = document.createElement('div');
+            var jqButton = $(button);
+            var buttonCss = {
+                'border-radius':'15px',
+                'padding':'5px 10px',
+                'background':'#fd4c5b',
+                'display':'inline-block',
+                'cursor':'pointer',
+                'margin-left':'20px',
+                'color':'#fff'
+            };
+            if(video){
+                buttonCss.margin = '20px 5px'
+            }
+            jqButton.css(buttonCss);
+            jqButton.attr('onclick',"lottery()");
+            jqButton.text('开始抽奖');
+            $('#art-operate').append(button.outerHTML);
+            if(video){
+                $('.banana').after(button.outerHTML);
+            }
+        }
     })
-    if(document.querySelector(".article-list")){
-         setTimeOut(()=>{
-        var observer = new MutationObserver(function (mutationsList) {
+})
+if(document.querySelector(".article-list")){
+ setTimeOut(()=>{
+    var observer = new MutationObserver(function (mutationsList) {
         var articleset = new Set()
         document.querySelectorAll('.article-item').forEach(e => {
-              if(!articleset.has(e.dataset.id)){
-                articleset.add(e.dataset.id)
-              } else{
-                e.style.display = 'none'
-                e.nextElementSibling.style.display = 'none'
-              }
-            })
-        });
-        observer.observe(document.querySelector(".article-list"),{
-            childList:true
-        })
-    },1500)    }
+          if(!articleset.has(e.dataset.id)){
+            articleset.add(e.dataset.id)
+        } else{
+            e.style.display = 'none'
+            e.nextElementSibling.style.display = 'none'
+        }
+    })
+    });
+    observer.observe(document.querySelector(".article-list"),{
+        childList:true
+    })
+},1500)    
+}
 })();
